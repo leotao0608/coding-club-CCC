96a8d2d856cde412ff934c2ede0734aec257ef23
leotao0608 <leotao0608@gmail.com>
2025-08-08 23:49:29 -0600

    Initial commit

---
+++ junior/script.js
@@ -0,0 +1,21 @@
+/*junior*/
+const yearSelect = document.getElementById('year-select');
+const problemsDiv = document.getElementById('problems');
+
+yearSelect.addEventListener('change', function() {
+  if (this.value) {
+    problemsDiv.style.display = 'block';
+  } else {
+    problemsDiv.style.display = 'none';
+  }
+});
+
+problemsDiv.addEventListener('click', function(e) {
+  if (e.target.tagName === 'BUTTON') {
+    const problemNum = e.target.getAttribute('data-problem');
+    const year = yearSelect.value;
+    if (year && problemNum) {
+      window.location.href = `./${year}j${problemNum}/index.html`;
+    }
+  }
+});
